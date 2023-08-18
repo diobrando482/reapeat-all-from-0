bac85b948fa7d580eed52ce910e97ac1d260b25b
diobrando482 <122156810+diobrando482@users.noreply.github.com>
2023-08-19 00:29:20 +0600

    obuchenie

---
+++ 1/main.js
@@ -0,0 +1,67 @@
+// //Object
+// const myCity ={
+//     city:'New York',
+//     country:'USA',
+//     popular:true 
+// }
+// myCity.city = 'Vashington'
+// //вложенность
+// const friendCity = {
+//     city:'Washington',
+//     info:{
+//         isPopular:true,
+//         country:'USA'
+//     }
+// } 
+// console.log(friendCity.info.country);
+// //удаление какого-то элемента в обьекте
+// delete friendCity.city
+// console.log(myCity)
+// //использование переменных 
+// const name = 'bogdan'
+// const postqty = 23
+// const userProfile = {
+//     name:name,
+//     postqty:postqty,
+//     cool:false
+// }
+// console.log(userProfile)
+//метод обьекта
+// const myCity = {
+//     city:'New York',
+//     cityGreeting:function(){
+//         console.log('Greetings');
+//     }
+// }
+// myCity.cityGreeting()
+// конвертирование json в Object js
+// var json_str = '{"имя": "Иван", "возраст": 30, "женат": false}';
+
+
+// var data = JSON.parse(json_str);
+
+// console.log(data);  
+//stringfy обьект в json
+
+//function
+// let a = 5
+// let b = 3
+// let c
+// c = a+b
+// console.log(c);
+// a = 8
+// b=12
+// c= a+b
+// console.log(c);
+// function sum (a,b){
+//     const c = a+b
+//     console.log(c);
+// }
+// sum(8,12)
+//  function myFn(a,b){
+//     let c 
+//     a=a+1
+//     c=a+b
+//     return c
+//  }
+//  console.log(myFn(10,3))
